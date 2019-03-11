96d801845b9751ce123a4ed183f87c7aa9c24eab
Jorge Yau <codenameyau@gmail.com>
2019-03-11 16:38:19 -0400

    Add coach states

--- src/components/Clippy/Clippy.stories.js
+++ src/components/Clippy/Clippy.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { storiesOf } from '@storybook/react';
 import {
-  withKnobs
+  withKnobs, select
 } from '@storybook/addon-knobs/react';
 
 import GithubLink from 'addons/GithubLink';
@@ -23,6 +23,24 @@ stories.add('demo', () => (
     <Clippy />
   </ClippyContainer>
 ));
+
+stories.add('interactive', () => {
+  const clippyStates = [
+    'appearing',
+    'smiling',
+    'smirking',
+  ];
+
+  const smirking = 'smiling';
+
+  return (
+    <ClippyContainer>
+      <Clippy
+        clippyState={select('Clippy State', clippyStates, smirking)}
+      />
+    </ClippyContainer>
+  )
+});
 
 stories.add('source code', () => (
   <div>
