86df49d91faf3b46e7bb6f0aea730cc251ef839a
Jorge Yau <codenameyau@gmail.com>
2019-07-04 14:26:43 -0400

    Add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: true,
+  tabWidth: 2,
+  semi: true,
+  tabs: false,
+  singleQuote: true,
+  printWidth: 80,
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
