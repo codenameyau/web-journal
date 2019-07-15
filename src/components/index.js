b0ef169ce69cfa9415bba1f1f3817f2132b8fcaf
Jorge Yau <codenameyau@gmail.com>
2019-07-15 14:28:27 -0400

    Fix android browser list warning

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as Clippy } from './components/Clippy/Clippy';
-export { default as ProgressIndicator } from './components/ProgressIndicator/ProgressIndicator';
-export { default as Snow } from './components/Snow/Snow';
+export { default as Clippy } from './Clippy/Clippy';
+export { default as ProgressIndicator } from './ProgressIndicator/ProgressIndicator';
+export { default as Snow } from './Snow/Snow';
