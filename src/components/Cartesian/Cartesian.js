1ebb87aecf0c62ec18cff7b5c80fc3e424a89b7a
Jorge Yau <codenameyau@gmail.com>
2019-07-26 17:36:50 -0400

    Add Cartesian

--- src/components/Cartesian/Cartesian.js
+++ src/components/Cartesian/Cartesian.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { space } from 'styled-system';
+
+export const CartesianGrid = styled.div`
+	display: grid;
+	grid-template-columns: ${({ cols = 4 }) => `repeat(${cols}, 1fr)`};
+	gap: 0.1em;
+	margin: auto;
+	${space}
+`;
+
+export const CartesianGridItem = styled.div`
+	border: 1px solid #eee;
+	padding: 0.5em;
+`;
 
 export const getCartesianProduct = arrays => {
 	if (!arrays || !arrays.length) {
@@ -10,7 +25,7 @@ export const getCartesianProduct = arrays => {
 		(acc, value) =>
 			acc
 				.map(x => value.map(y => x.concat(y)))
-				.reduce((acc, b) => acc.concat(b), []),
+				.reduce((acc, val) => acc.concat(val), []),
 		[[]]
 	);
 };
@@ -29,18 +44,52 @@ export const getCartesianProps = (props = {}) => {
 	});
 };
 
-export const Cartesian = ({ component, ...props }) => {
+export const getJSX = (displayName, props = {}) => {
+	const propsAttributes = Object.keys(props).reduce((acc, key) => {
+
+	}, '');
+
+	const jsx = `
+		<${displayName}>
+			${props.children}
+		</${displayName}>
+	`;
+
+	return jsx;
+};
+
+export const Cartesian = ({ component, props, ...restProps }) => {
 	const Component = component;
-
 	const cartesianProps = getCartesianProps(props);
-	console.log(props, cartesianProps);
+	const cols = restProps.cols || 4;
 
-	return <Component>{props.children}</Component>;
+	const copyComponent = (e, idx) => {
+		console.log(e, idx, component);
+		console.log(React.renderToStaticMarkup)
+		// cartesianProps(idx);
+		// debugger;
+	};
+
+	return (
+		<CartesianGrid cols={cols} {...restProps}>
+			{cartesianProps.map(({ children, ...cartesianProps }, idx) => (
+				<CartesianGridItem
+					key={idx}
+					onClick={e => {
+						copyComponent(e, idx)
+					}}
+				>
+					<Component {...cartesianProps}>{children}</Component>
+				</CartesianGridItem>
+			))}
+		</CartesianGrid>
+	);
 };
 
 Cartesian.propTypes = {
 	component: PropTypes.elementType,
 	props: PropTypes.object,
+	cols: PropTypes.number,
 };
 
 export default Cartesian;
